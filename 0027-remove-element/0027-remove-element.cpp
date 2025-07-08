ccb5f11803a5608828f0551f1fe0cb2970462b0f
sahilbakshi3 <97842567+sahilbakshi3@users.noreply.github.com>
2025-07-08 17:57:58 +0530

    Time: 0 ms (100%), Space: 11.9 MB (5.14%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,15 +3,29 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int n = nums.size();
 
-        int k = 0;
+        // int k = 0;
+
+        // for(int i = 0; i< n; i++){
+        //     if(nums[i] != val){
+        //         nums[k] = nums[i];
+        //         k++;
+        //     }
+        // }
+        // return k;
+
+        vector<int> res;
 
         for(int i = 0; i< n; i++){
             if(nums[i] != val){
-                nums[k] = nums[i];
-                k++;
+                res.push_back(nums[i]);
             }
         }
-        return k;
-        
+
+        nums.clear();
+
+        for(int i = 0; i< res.size(); i++){
+            nums.push_back(res[i]);
+        }
+        return nums.size();
     }
 };
