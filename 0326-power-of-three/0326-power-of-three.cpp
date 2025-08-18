a37f35b2ac6f94f70a58be402be5d15f88051140
sahilbakshi3 <97842567+sahilbakshi3@users.noreply.github.com>
2025-08-18 13:17:56 +0530

    Time: 5 ms (33.54%), Space: 8.9 MB (77.82%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        // if(n == 0){
+        //   return false;
+        // }
+
+        // while(n%3 == 0){
+        //   n = n/3;
+        // }
+        // if(n == 1){
+        //   return true;
+        // }
+        // else{
+        //   return false;
+        // }
+
+        // Recursive
+
+        if(n == 0) return false;
+
+        else if(n == 1) return true;
+
+        if(n%3 == 0 && isPowerOfThree(n/3)){
+          return true;
+        }
+        else{
+          return false;
+        }
+        
+    }
+};
