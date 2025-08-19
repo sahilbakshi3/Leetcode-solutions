18fbe8ad88809e7c04c084e4a089dbfb4029713d
sahilbakshi3 <97842567+sahilbakshi3@users.noreply.github.com>
2025-08-20 02:31:34 +0530

    Time: 0 ms (100%), Space: 15 MB (54.68%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {        
-        int row = 0;
+        // int row = 0;
 
-        auto lambda = [&](int sum, vector<int> &temp){
-            sum += temp[row];
+        // auto lambda = [&](int sum, vector<int> &temp){
+        //     sum += temp[row];
 
-            if(row != temp.size() - row -1){
-                sum += temp[temp.size() - row - 1];
+        //     if(row != temp.size() - row -1){
+        //         sum += temp[temp.size() - row - 1];
+        //     }
+
+        //     row++;
+
+        //     return sum;
+        // };
+
+
+        // int res = accumulate(mat.begin(), mat.end(), 0, lambda);
+
+        // return res;
+
+        int n = mat.size();
+        int sum = 0;
+
+        for(int i = 0; i< n; i++){
+            sum += mat[i][i];
+
+            if(i != n - i - 1){
+                sum += mat[i][n - i - 1];
             }
-
-            row++;
-
-            return sum;
-        };
-
-
-        int res = accumulate(mat.begin(), mat.end(), 0, lambda);
-
-        return res;
+        }
+        return sum;
     }
 };
