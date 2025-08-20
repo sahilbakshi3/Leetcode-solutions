c8eeb454749650ad491947ab77ab98846b2fe868
sahilbakshi3 <97842567+sahilbakshi3@users.noreply.github.com>
2025-08-20 12:20:09 +0530

    Time: 3 ms (44.66%), Space: 7.9 MB (31.84%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        // return n-1;
+        int cnt = 0;
+
+        while(n > 1){
+            if(n%2 == 0){
+                cnt += n/2;
+                n = n/2;
+            }
+            else{
+                cnt += (n-1)/2;
+                n = (n-1)/2 + 1;
+            }
+        }
+        return cnt;
+
+    }
+};
