3e0913da0024bb355d08515777944eb025b1682a
sahilbakshi3 <97842567+sahilbakshi3@users.noreply.github.com>
2025-09-29 13:51:15 +0530

    Time: 0 ms (100%), Space: 56.1 MB (95.25%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(!head){
+        return head;
+    }
+
+    let temp = head;
+
+    while(temp){
+        if(temp.next && temp.val === temp.next.val){
+            temp.next = temp.next.next;
+        }
+        else{
+            temp = temp.next;
+        }
+    }
+    return head;
+};
