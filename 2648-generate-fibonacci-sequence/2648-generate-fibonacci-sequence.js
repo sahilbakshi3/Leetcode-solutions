79fda54808e5a88f85983e42fb9f9702cc4f8dd0
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-01-08 17:52:52 +0530

    Time: 44 ms (60.54%), Space: 54.1 MB (47.31%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let prev = 0, curr = 1;
+    while(true){
+        yield prev
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
