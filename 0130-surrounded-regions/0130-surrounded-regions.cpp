80aeb1c0bd3492719c2136d10d5ec428a11412b0
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-01-29 22:06:05 +0530

    Time: 0 ms (100%), Space: 14.6 MB (47.6%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    
+    void dfs(int row, int col, vector<vector<char>>& mat, vector<vector<int>> &vis){
+        vis[row][col] = 1;
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, -1, 0, 1};
+
+        int n = mat.size();
+        int m = mat[0].size();
+
+        for(int i = 0; i< 4; i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && mat[nrow][ncol] == 'O'){
+                dfs(nrow, ncol, mat, vis);
+            }
+        }
+
+    }
+    
+    void solve(vector<vector<char>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+
+        for(int i = 0; i< n; i++){
+            if(mat[i][0] == 'O'){
+                vis[i][0] = 1;
+                dfs(i, 0, mat, vis);
+            }
+            if(mat[i][m-1] == 'O'){
+                vis[i][m-1] = 1;
+                dfs(i, m-1, mat, vis);
+            }
+        }
+
+        for(int j = 0; j< m; j++){
+            if(mat[0][j] == 'O'){
+                vis[0][j] = 1;
+                dfs(0, j, mat, vis);
+            }
+            if(mat[n-1][j] == 'O'){
+                vis[n-1][j] = 1;
+                dfs(n-1, j, mat, vis);
+            }
+        }
+
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j< m; j++){
+                if(mat[i][j] == 'O' && vis[i][j] == 0){
+                    mat[i][j] = 'X';
+                }
+            }
+        }
+
+    }
+};
