5afb334f2ac8aedfa2754366fd9458365f03acf7
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-01-30 15:33:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
@@ -29,6 +29,44 @@ class Solution {
         
     }
     
+    void bfs(int row0, int col0,
+             vector<vector<int>>& grid,
+             vector<vector<int>>& vis,
+             vector<pair<int,int>>& temp) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        queue<pair<int,int>> q;
+        q.push({row0, col0});
+        vis[row0][col0] = 1;
+        
+        int delrow[4] = {-1, 0, 1, 0};
+        int delcol[4] = {0, -1, 0, 1};
+        
+        while(!q.empty()) {
+            auto [row, col] = q.front();
+            q.pop();
+            
+            // store relative position
+            temp.push_back({row - row0, col - col0});
+            
+            for(int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                
+                if(nrow >= 0 && nrow < n &&
+                   ncol >= 0 && ncol < m &&
+                   grid[nrow][ncol] == 1 &&
+                   vis[nrow][ncol] == 0) {
+                    
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
+    }
+    
     int countDistinctIslands(vector<vector<int>>& grid) {
         // code here
         int n = grid.size();
@@ -43,7 +81,8 @@ class Solution {
             for(int j = 0; j< m; j++){
                 if(vis[i][j] == 0 && grid[i][j] == 1){
                     vector<pair<int, int>> temp;
-                    dfs(i, j, grid, vis, temp, i, j);
+                    // dfs(i, j, grid, vis, temp, i, j);
+                    bfs(i, j, grid, vis, temp);
                     st.insert(temp);
                 }
             }
