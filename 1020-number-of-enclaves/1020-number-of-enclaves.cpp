7064e31ea743439a012e6cef82bb6a1ba7fcd6cf
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-01-30 15:00:37 +0530

    Time: 10 ms (45.37%), Space: 38 MB (19.75%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,53 +1,105 @@
 class Solution {
 public:
+
+    int n, m;
+    vector<vector<int>> vis;
+
+    int delrow[4] = {-1, 0, 1, 0};
+    int delcol[4] = {0, -1, 0, 1};
+
+    void dfs(int row, int col, vector<vector<int>>& mat) {
+        vis[row][col] = 1;
+
+        for(int i = 0; i < 4; i++) {
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+
+            if(nrow >= 0 && nrow < n &&
+               ncol >= 0 && ncol < m &&
+               mat[nrow][ncol] == 1 &&
+               vis[nrow][ncol] == 0) {
+
+                dfs(nrow, ncol, mat);
+            }
+        }
+    }
+
     int numEnclaves(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
+        // int n = mat.size();
+        // int m = mat[0].size();
 
-        queue<pair<int, int>> q;
+        // queue<pair<int, int>> q;
 
-        vector<vector<int>> vis(n, vector<int> (m, 0));
+        // vector<vector<int>> vis(n, vector<int> (m, 0));
 
-        for(int i = 0; i< n; i++){
-            for(int j = 0; j< m; j++){
-                if(i == 0 || j == 0 || i == n-1 || j == m-1){
-                    if(mat[i][j] == 1){
-                        q.push({i, j});
-                        vis[i][j] = 1;
+        // for(int i = 0; i< n; i++){
+        //     for(int j = 0; j< m; j++){
+        //         if(i == 0 || j == 0 || i == n-1 || j == m-1){
+        //             if(mat[i][j] == 1){
+        //                 q.push({i, j});
+        //                 vis[i][j] = 1;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // int delrow[] = {-1, 0, 1, 0};
+        // int delcol[] = {0, -1, 0, 1};
+
+        // while(!q.empty()){
+        //     int row = q.front().first;
+        //     int col = q.front().second;
+        //     q.pop();
+
+        //     for(int i = 0; i< 4; i++){
+        //         int nrow = row + delrow[i];
+        //         int ncol = col + delcol[i];
+
+        //         if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && mat[nrow][ncol] == 1){
+        //             q.push({nrow, ncol});
+        //             vis[nrow][ncol] = 1;
+        //         }
+        //     }
+
+        // }
+
+        // int cnt = 0;
+
+        // for(int i = 0; i< n; i++){
+        //     for(int j = 0; j< m; j++){
+        //         if(mat[i][j] == 1 && vis[i][j] == 0){
+        //             cnt++;
+        //         }
+        //     }
+        // }
+        // return cnt;
+
+        n = mat.size();
+        m = mat[0].size();
+
+        vis.assign(n, vector<int>(m, 0));
+
+        // DFS from boundary 1s
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+                    if(mat[i][j] == 1 && vis[i][j] == 0) {
+                        dfs(i, j, mat);
                     }
                 }
             }
         }
 
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, -1, 0, 1};
-
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-
-            for(int i = 0; i< 4; i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && mat[nrow][ncol] == 1){
-                    q.push({nrow, ncol});
-                    vis[nrow][ncol] = 1;
-                }
-            }
-
-        }
-
+        // Count enclaves
         int cnt = 0;
-
-        for(int i = 0; i< n; i++){
-            for(int j = 0; j< m; j++){
-                if(mat[i][j] == 1 && vis[i][j] == 0){
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(mat[i][j] == 1 && vis[i][j] == 0) {
                     cnt++;
                 }
             }
         }
         return cnt;
+
     }
 };
