740ae2a710cb894c84d3f0e66303800cff2681fa
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-01-31 19:59:17 +0530

    Time: 0 ms (100%), Space: 17.3 MB (89.97%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,38 +1,72 @@
 class Solution {
 public:
+    
+    vector<int> color;
+
+    bool dfs(int node, vector<vector<int>>& graph){
+        for(auto &it: graph[node]){
+            if(color[it] == -1){
+                color[it] = !color[node];
+
+                if(!dfs(it, graph)){
+                    return false;
+                }
+                
+            }
+
+            else if(color[it] == color[node]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
     bool isBipartite(vector<vector<int>>& graph) {
+        // int n = graph.size();
+
+        // queue<int> q;
+
+        // vector<int> color(n, -1);
+
+        // for(int i = 0; i< n; i++){
+        //     if(color[i] != -1){
+        //         continue;
+        //     }
+
+        //     color[i] = 0;
+        //     q.push(i);
+
+        //     while(!q.empty()){
+        //         int node = q.front();
+        //         q.pop();
+
+        //         for(auto &it: graph[node]){
+        //             if(color[it] == -1){
+        //                 color[it] = !color[node];
+        //                 q.push(it);
+        //             }
+        //             else if(color[it] == color[node]){
+        //                 return false;
+        //             }
+        //         }
+        //     }
+
+        // }
+
+        // return true;
+
         int n = graph.size();
 
-        queue<int> q;
-
-        vector<int> color(n, -1);
+        color.assign(n, -1);
 
         for(int i = 0; i< n; i++){
-            if(color[i] != -1){
-                continue;
-            }
-
-            color[i] = 0;
-            q.push(i);
-
-            while(!q.empty()){
-                int node = q.front();
-                q.pop();
-
-                for(auto &it: graph[node]){
-                    if(color[it] == -1){
-                        color[it] = !color[node];
-                        q.push(it);
-                    }
-                    else if(color[it] == color[node]){
-                        return false;
-                    }
+            if(color[i] == -1){
+                color[i] = 0;
+                if(!dfs(i, graph)){
+                    return false;
                 }
             }
-
         }
-
         return true;
-
     }
 };
