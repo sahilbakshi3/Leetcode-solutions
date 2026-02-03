ef1b26b271c65d315964cae5b10be7aa8d0fbb61
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-02-03 18:30:47 +0530

    Time: 0 ms (100%), Space: 14 MB (10.52%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    int n, m;
+
+    void bfs(int r, int c, vector<vector<char>> &board){
+        board[r][c] = '.';
+        queue<pair<int, int>> q;
+        q.push({r, c});
+
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, -1, 0, 1};
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i = 0; i< 4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && board[nrow][ncol] == 'X'){
+                    board[nrow][ncol] = '.';
+                    q.push({nrow, ncol});
+                }
+
+            }
+
+        }
+
+    }
+    
+    int countBattleships(vector<vector<char>>& board) {
+        n = board.size();
+        m = board[0].size();
+
+        int cnt = 0;
+
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j< m; j++){
+                if(board[i][j] == 'X'){
+                    cnt++;
+                    bfs(i, j, board);
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
