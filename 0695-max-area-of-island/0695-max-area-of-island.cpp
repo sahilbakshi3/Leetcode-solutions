1c7a158a30e001f2d0afc810bc90c67925bb6aee
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-02-03 14:35:51 +0530

    Time: 0 ms (100%), Space: 27.3 MB (90.46%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -31,6 +31,24 @@ public:
         }
         return area;
     }
+
+    int dfs(int r, int c, vector<vector<int>> &grid){
+
+        if(r < 0 || r >= n || c < 0 || c >= m || grid[r][c] == 0){
+            return 0;
+        }
+
+        grid[r][c] = 0;
+        int area = 1;
+        
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, -1, 0, 1};
+
+        for(int i = 0; i< 4; i++){
+            area += dfs(r + delrow[i], c + delcol[i], grid);
+        }
+        return area;
+    }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         n = grid.size();
@@ -42,7 +60,8 @@ public:
         for(int i = 0; i< n; i++){
             for(int j = 0; j< m; j++){
                 if(grid[i][j] == 1){
-                    maxArea = max(maxArea, bfs(i, j, grid));
+                    // maxArea = max(maxArea, bfs(i, j, grid));
+                    maxArea = max(maxArea, dfs(i, j, grid));
                 }
             }
         }
