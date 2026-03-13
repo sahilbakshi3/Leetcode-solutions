5608b571ca966a296b2b227c4ec3a85e88b8f132
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-03-13 14:56:11 +0530

    Time: 161 ms (21.29%), Space: 32.3 MB (20.65%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,43 +1,74 @@
 class Solution {
 public:
     
-    typedef long long ll;
+    // typedef long long ll;
 
-    bool check(ll mid, vector<int>& workerTimes, int &mountainHeight){
-        ll h = 0;
+    // bool check(ll mid, vector<int>& workerTimes, int &mountainHeight){
+    //     ll h = 0;
 
-        for(auto t: workerTimes){
-            h += (ll)(sqrt(2.0*mid/t + 0.25) - 0.5);
+    //     for(auto t: workerTimes){
+    //         h += (ll)(sqrt(2.0*mid/t + 0.25) - 0.5);
 
-            if(h >= mountainHeight){
-                return true;
-            }
-        }
-        return h >= mountainHeight;
-    }
+    //         if(h >= mountainHeight){
+    //             return true;
+    //         }
+    //     }
+    //     return h >= mountainHeight;
+    // }
     
-    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+    long long minNumberOfSeconds(int h, vector<int>& t) {
 
-        int maxTime = *max_element(workerTimes.begin(), workerTimes.end());
+        // int maxTime = *max_element(workerTimes.begin(), workerTimes.end());
 
-        ll l = 1;
-        ll r = 1ll * maxTime * mountainHeight * (mountainHeight+1)/2;
+        // ll l = 1;
+        // ll r = 1ll * maxTime * mountainHeight * (mountainHeight+1)/2;
 
-        ll result = 0;
+        // ll result = 0;
 
-        while(l <= r){
+        // while(l <= r){
 
-            ll mid = l + (r - l)/2;
+        //     ll mid = l + (r - l)/2;
 
-            if(check(mid, workerTimes, mountainHeight)){
-                result = mid;
-                r = mid - 1;
+        //     if(check(mid, workerTimes, mountainHeight)){
+        //         result = mid;
+        //         r = mid - 1;
 
-            }
-            else{
-                l = mid + 1;
+        //     }
+        //     else{
+        //         l = mid + 1;
+        //     }
+        // }
+        // return result;
+
+        priority_queue<pair<long long, pair<int,int>>, 
+        vector<pair<long long, pair<int,int>>>, 
+        greater<pair<long long, pair<int,int>>>> pq;
+
+        for(int i = 0; i < t.size(); i++){
+            long long tm = t[i];
+            pq.push({tm, {i, 1}});
+        }
+
+        long long res = 0;
+
+        while(h > 0){
+            auto cur = pq.top();
+            pq.pop();
+
+            long long tm = cur.first;
+            int id = cur.second.first;
+            int x = cur.second.second;
+
+            res = tm;
+            h--;
+
+            if(h > 0){
+                long long nx = x + 1;
+                long long nt = (long long)t[id] * (nx * (nx + 1) / 2);
+                pq.push({nt, {id, (int)nx}});
             }
         }
-        return result;
+
+        return res;
     }
 };
