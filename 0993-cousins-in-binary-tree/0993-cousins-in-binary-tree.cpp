90806464bc54c6c0aed1a16a6e5d5f5bf73e3399
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-04-02 21:54:46 +0530

    Time: 0 ms (100%), Space: 14.1 MB (48.59%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCousins(TreeNode* root, int x, int y) {
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            bool foundX = false, foundY = false;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front(); q.pop();
+
+                if (node->val == x) foundX = true;
+                if (node->val == y) foundY = true;
+
+                if (node->left && node->right) {
+                    if ((node->left->val == x && node->right->val == y) ||
+                        (node->left->val == y && node->right->val == x)) {
+                        return false;
+                    }
+                }
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            if (foundX && foundY) return true;
+            if (foundX || foundY) return false;
+        }
+
+        return false;
+    }
+};
