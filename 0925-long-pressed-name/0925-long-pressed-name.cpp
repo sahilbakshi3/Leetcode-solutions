8d51d0debef0d5d04da6dcf7f31e761205cb1101
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-04-21 12:57:27 +0530

    Time: 0 ms (100%), Space: 8.7 MB (75.62%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int n = name.length();
+        int m = typed.length();
+
+        int i = 0, j = 0;
+
+        while(j < m ) {
+            if(i < n && name[i] == typed[j]) {
+                i++;
+                j++;
+            }
+
+            else if(j > 0 && typed[j] == typed[j-1]) {
+                j++;
+            }
+            else {
+                return false;
+            }
+
+        }
+
+        return i == n;
+
+    }
+};
