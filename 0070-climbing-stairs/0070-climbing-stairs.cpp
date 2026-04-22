a3ed871ca16cbe46a889b53dda5ebefcd97f6e93
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-04-22 13:02:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (45.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -15,6 +15,16 @@ public:
         // return climbStairs(n-1) + climbStairs(n-2);
         if(n<=2) return n;
         vector<int> dp(n+1, -1);
-        return solve(n, dp);
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i<= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+
+        // return solve(n, dp);
     }
 };
