355601a6cf8736caef64324360c486fb59118b82
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-05-20 09:08:15 +0530

    Implement solution for minimum cost to connect sticks

---
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int connectSticks(vector<int>& sticks) {
+      
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (int stick : sticks) {
+            pq.push(stick);
+        }
+
+        int cost = 0;
+
+        while (pq.size() > 1) {
+            int first = pq.top();
+            pq.pop();
+
+            int second = pq.top();
+            pq.pop();
+
+            int sum = first + second;
+
+            cost += sum;
+
+            pq.push(sum);
+        }
+
+        return cost;
+    }
+};
