e093fd1018f2fe7531013ae13098cec6985d189d
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-05-22 15:41:50 +0530

    Implement upper bound function for array search

---
+++ 2774-array-upper-bound/2774-array-upper-bound.js
@@ -0,0 +1,20 @@
+var upperBound = function(nums, target) {
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] <= target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    let index = left - 1;
+
+    return index >= 0 && nums[index] === target ? index : -1;
+};
