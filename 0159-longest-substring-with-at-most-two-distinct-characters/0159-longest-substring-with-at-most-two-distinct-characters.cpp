0b572ac2044a15660e3be72ded05fcd24c702d20
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-05-23 08:28:41 +0530

    Implement solution for longest substring with two distinct characters

---
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+
+        unordered_map<char, int> freq;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            freq[s[right]]++;
+            while (freq.size() > 2) {
+
+                freq[s[left]]--;
+                if (freq[s[left]] == 0) {
+                    freq.erase(s[left]);
+                  
+                }
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
