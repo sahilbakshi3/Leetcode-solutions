a63a85f9150be82a5d1af9a88598759f5f0c6a73
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-05-25 10:12:18 +0530

    Time: 0 ms (100%), Space: 11 MB (86.04%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+      vector<int> freq(1001, 0);
+
+        for (int num : arr1) {
+            freq[num]++;
+        }
+
+        vector<int> ans;
+
+        for (int num : arr2) {
+
+            while (freq[num] > 0) {
+
+                ans.push_back(num);
+                freq[num]--;
+            }
+        }
+
+        for (int i = 0; i <= 1000; i++) {
+
+            while (freq[i] > 0) {
+
+                ans.push_back(i);
+                freq[i]--;
+            }
+        }
+
+        return ans;  
+    }
+};
