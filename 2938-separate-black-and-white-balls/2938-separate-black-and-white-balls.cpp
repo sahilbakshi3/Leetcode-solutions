f205016fbe5a7b908101ffa31fa5e3c30c92d7d1
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-06-05 21:11:08 +0530

    Time: 7 ms (51.29%), Space: 17.3 MB (37.86%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        int n = s.length();
+
+        long long ans = 0;
+        long long temp = 0;
+
+        for(int i = 0; i< n; i++){
+            if(s[i] == '1'){
+                temp++;
+            }
+            else{
+                ans += temp;
+            }
+        }
+
+        return ans;
+    }
+};
