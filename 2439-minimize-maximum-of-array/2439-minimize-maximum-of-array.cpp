3639dbc988e884e12c674d2b0b72dd3e579cf25b
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-06-06 10:04:10 +0530

    Time: 0 ms (100%), Space: 75.1 MB (69.29%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        int n = nums.size();
+
+        long long sum = 0, ans = 0;
+
+        for(int i = 0; i< n; i++){
+            sum += nums[i];
+
+            long long temp = (sum + i) / (i + 1);
+
+            ans = max(ans, temp);
+        }
+
+        return (int)ans;
+    }
+};
