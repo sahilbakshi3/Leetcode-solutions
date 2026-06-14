9da92d665b5fa7154aaf9c928866489e0709820e
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-06-14 14:53:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Unordered Set Hash Function/unordered-set-hash-function.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+// Complete the function
+long int HashMe(unordered_set<string> myset, string str) {
+
+    // Your code here
+    hash<string> h;
+    return h(str);
+}
