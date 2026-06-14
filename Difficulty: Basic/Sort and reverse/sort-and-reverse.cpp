21997d9cbdd11df44a26375ffa2b3d18ea2a77d9
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-06-14 19:29:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort and reverse/sort-and-reverse.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+void sortAndReverse(vector<int> &arr) {
+    // your code here
+    sort(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.end());
+    
+
+}
