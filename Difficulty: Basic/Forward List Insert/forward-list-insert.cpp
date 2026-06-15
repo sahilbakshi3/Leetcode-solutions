f6af65966e57479e0a1d3b043060ec60a5b0aa6d
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-06-15 11:58:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Forward List Insert/forward-list-insert.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+
+// arr: insert the elements of vector to forwardList
+// Complete the function
+forward_list<int> insertIntoFL(vector<int>& arr) {
+    // your code here
+    forward_list<int> fl;
+    
+    for(int i = 0; i< arr.size(); i++){
+        fl.push_front(arr[i]);
+    }
+    
+    return fl;
+    // Use push_front() method to push the elements in a forward list and return the
+    // list
+}
