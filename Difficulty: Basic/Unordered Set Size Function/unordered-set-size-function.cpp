fa7cb4118d9369dbd8ea9bc872084ab850f01faa
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-06-15 12:09:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Unordered Set Size Function/unordered-set-size-function.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+// Complete the function
+int sizeMe(unordered_set<string> myset) {
+
+    // Your code here
+    return myset.size();
+    // Use size function
+}
