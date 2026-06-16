b1b8b700cdaa71f883554576dba2f4c6ba953657
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-06-16 12:37:03 +0530

    Time: 0 ms (100%), Space: 29.4 MB (67.39%) - LeetHub

---
+++ 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumSumSubarray(vector<int>& nums, int l, int r) {
+        int n = nums.size();
+
+        int ans = INT_MAX;
+
+        for(int i = l; i <= r; i++){
+            int sum = 0;
+            for(int j = 0; j < i; j++){
+                sum += nums[j];
+            }
+
+            if(sum > 0){
+                ans = min(ans, sum);
+            }
+
+            for(int j = i; j < n; j++){
+                sum += nums[j] - nums[j - i];
+
+                if(sum > 0){
+                    ans = min(ans, sum);
+                }
+            }
+        }
+
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
