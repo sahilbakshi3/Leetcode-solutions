acd509355cad2b8aab23c7ea688830ae1245fdef
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-06-18 13:51:41 +0530

    Add script to transpose lines of a file

---
+++ 0194-transpose-file/0194-transpose-file.sh
@@ -0,0 +1,13 @@
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+        if (NR == 1)
+            res[i] = $i;
+        else
+            res[i] = res[i] " " $i;
+    }
+}
+END {
+    for (i = 1; i <= NF; i++)
+        print res[i];
+}' file.txt
