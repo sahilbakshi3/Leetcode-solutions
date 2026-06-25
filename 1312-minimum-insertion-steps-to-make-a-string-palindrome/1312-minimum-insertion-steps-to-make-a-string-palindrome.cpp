261f3fc181467abe7a8b0f1e6be974ff5b656e14
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-06-25 13:38:11 +0530

    Time: 64 ms (9.64%), Space: 31.7 MB (15.18%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
     
-    int solve(int i, int j, string &s, vector<vector<int>> &dp) {
-        if(i >= j){
+    // int solve(int i, int j, string &s, vector<vector<int>> &dp) {
+    //     if(i >= j){
+    //         return 0;
+    //     }
+
+    //     if(s[i] == s[j]) {
+    //         return dp[i][j] = solve(i + 1, j - 1, s, dp);
+    //     }
+
+    //     if(dp[i][j] != -1){
+    //         return dp[i][j];
+    //     }
+
+    //     return dp[i][j] = 1 + min(solve(i + 1, j, s, dp), solve(i, j - 1, s, dp));
+    // }
+
+    int solve(int i, int j, string &s, string &rev, vector<vector<int>> &dp) {
+        if(i == s.length() || j == rev.length()){
             return 0;
         }
 
-        if(s[i] == s[j]) {
-            return dp[i][j] = solve(i + 1, j - 1, s, dp);
-        }
-
-        if(dp[i][j] != -1){
+        if(dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        return dp[i][j] = 1 + min(solve(i + 1, j, s, dp), solve(i, j - 1, s, dp));
+        if(s[i] == rev[j]) {
+            return dp[i][j] = 1 + solve(i + 1, j + 1, s, rev, dp);
+        }
+
+        return dp[i][j] = max(solve(i + 1, j, s, rev, dp), solve(i, j + 1, s, rev, dp));
     }
     
     int minInsertions(string s) {
         int n = s.length();
 
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
         vector<vector<int>> dp(n, vector<int> (n, -1));
 
-        return solve(0, n - 1, s, dp);
+        int ans = solve(0, 0, s, rev, dp);
+
+        return n - ans;
+
+        // vector<vector<int>> dp(n, vector<int> (n, -1));
+
+        // return solve(0, n - 1, s, dp);
     }
 };
