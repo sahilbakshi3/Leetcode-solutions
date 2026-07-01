ed6bc894fb1cf520583d85aa5eac1486a4f6caa2
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-07-01 18:32:29 +0530

    Create 0487-max-consecutive-ones-ii.cpp

---
+++ 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+
+        int left = 0;
+        int zeros = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+
+            if (nums[right] == 0)
+                zeros++;
+
+            while (zeros > 1) {
+                if (nums[left] == 0)
+                    zeros--;
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
